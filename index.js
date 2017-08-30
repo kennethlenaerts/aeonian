09fb899303566673dd4ee66e327605efecec8cec
Kenneth Lenaerts <Lenaerts.Kenneth@gmail.com>
2017-08-30 14:09:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -222,8 +222,11 @@ exports.updateCloudFrontOrigin = (id, domain, environment, complete) => {
         updateParams.IfMatch = updateParams.ETag
         delete updateParams.ETag
 
-        let previous = updateParams.Origins.Items[0].DomainName.replace('.s3-website-us-east-1.amazonaws.com', '')
-        let current = domain.replace('.s3-website-us-east-1.amazonaws.com', '')
+        let previous = updateParams.Origins.Items[0].DomainName.replace(
+          '.s3.amazonaws.com',
+          '',
+        );
+        let current = domain.replace('.s3.amazonaws.com', '');
 
         updateParams.Origins.Items[0].DomainName = domain
         cloudfront.updateDistribution(updateParams, (terror, tdata) => {
